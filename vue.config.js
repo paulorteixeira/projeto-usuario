efc701dfa40c2a0889f6042a1f331a267cba0c68
Paulo Ricardo <1981928paulo@gmail.com>
2022-08-03 19:44:22 -0300

    Build and Deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === "production" ? "/projeto-usuario/" : "/",
+
 })
